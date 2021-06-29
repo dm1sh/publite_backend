c24a26ab291f2fc6efe365c44e858cfb1d546be4
dm1sh <me@dmitriy.icu>
2021-06-29 17:59:27 +0500

    Added epub file processing: currently epub file tokenization is ready

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+from .epub import epub2html
 
 app = FastAPI()
 
 @app.get('/')
 def root():
     return "Hello, World!"
+
+@app.post('/uploadfile/', )
+async def create_upload_file(file: UploadFile = File(...)):
+    if file.filename.endswith('.epub'):
+        content = await epub2html(file.file)
+    elif file.filename.endswith('.fb2'):
+        content = await fb22html(file.file)
+    else:
+        content = 'Error! Unsupported file type'
+    return HTMLResponse(content=content)
